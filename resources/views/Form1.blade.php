36ccc0c29a6c94c3ecb62b0b60d074d7534e3933
Zen <64142347+zensdev@users.noreply.github.com>
2023-07-21 14:09:59 +0700

    form

--- resources/views/Form1.blade.php
+++ resources/views/Form1.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
     <title>Bootstrap Example</title>
@@ -11,58 +11,309 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="http://codeseven.github.io/toastr/build/toastr.min.js"></script>
     <link href="http://codeseven.github.io/toastr/build/toastr.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="/css/app.css">
 </head>
 
 <body>
-    <h1>Create Post</h1>
-
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-            <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+    @yield('Navbar')
     <div class="container">
+        <h1>Create Post</h1>
         <form action="{{ route('customer.store') }}" method="POST" id="idForm">
             @csrf
-            <div class="field">
-                <label class="label">TenKH</label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="e.g. barryallen@example.com" name="TenKH">
+            <h2>Thông tin khách hàng</h2>
+
+            <div class="d-flex justify-content-between flex-column flex-md-row">
+                <div class="inputBox p-3">
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Tên khách hàng: </label>
+                        <div class="control">
+                            <input class="input form-control" type="text" placeholder="Nhập tên khách hàng" name="TenKH">
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Mã thuế: </label>
+                        <div class="control">
+                            <input class="input form-control" type="text" placeholder="Nhập mã thuế" name="MaThue">
+                        </div>
+                        <!-- <p class="help is-danger">{{ $errors->first('MaThue') }}</p> -->
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Mã BHXH: </label>
+                        <div class="control">
+                            <input class="input form-control" type="text" placeholder="Nhập mã BHXH" name="MaBHXH">
+                        </div>
+                    </div>
                 </div>
-                <p class="help is-danger">{{ $errors->first('TenKH') }}</p>
+
+                <div class="inputBox p-3">
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Tỉnh/ Thành phố: </label>
+                        <div class="control">
+                            <select id="provinceInput" class="form-control">
+
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Quận/ Huyện: </label>
+                        <div class="control">
+                            <select id="districtInput" class="form-control">
+
+                            </select>
+                        </div>
+                        <!-- <p class="help is-danger">{{ $errors->first('DiaChi') }}</p> -->
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Phường/ Xã: </label>
+                        <div class="control">
+                            <select id="wardInput" class="form-control">
+
+                            </select>
+                        </div>
+                        <!-- <p class="help is-danger">{{ $errors->first('DiaChi') }}</p> -->
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Số nhà: </label>
+                        <div class="control">
+                            <input class="input form-control" type="text" placeholder="e.g. 123" name="sonha">
+                        </div>
+                        <!-- <p class="help is-danger">{{ $errors->first('TenKH') }}</p> -->
+                    </div>
+                </div>
             </div>
-            <div class="field">
-                <label class="label">DiaChi</label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="e.g. barryallen@example.com" name="DiaChi">
+
+            <h2>Thông tin hợp đồng</h2>
+
+            <div class="d-flex justify-content-between flex-column flex-md-row">
+                <div class="inputBox p-3">
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Nhân viên lập hợp đồng: </label>
+                        <div class="control">
+                            <select id="staffInput" class="form-control">
+
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Ngày ký hợp đồng: </label>
+                        <div class="control">
+                            <input class="input form-control" type="date" name="ngaykyhd">
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Mã số hợp đồng: </label>
+                        <div class="control">
+                            <input class="input form-control" type="text" placeholder="13HD" name="mahd">
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Trạng thái đơn hàng: </label>
+                        <div class="control">
+                            <select id="orderStatusInput" class="form-control" name="trangthaidonhang">
+                                <option value="0">Chưa duyệt</option>
+                                <option value="1">Đã duyệt</option>
+                                <option value="2">Từ chối</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Loại đơn hàng: </label>
+                        <div class="control">
+                            <select id="orderTypeInput" class="form-control" name="loaidonhang">
+                                <option value="L1">Loại 1</option>
+                                <option value="L2">Loại 2</option>
+                                <option value="L3">Loại 3</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Dịch vụ: </label>
+                        <div class="control">
+                            <select id="serviceInput" class="form-control">
+
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Gói cước: </label>
+                        <div class="control">
+                            <select id="packInput" class="form-control">
+
+                            </select>
+                        </div>
+                    </div>
                 </div>
-                <p class="help is-danger">{{ $errors->first('DiaChi') }}</p>
-            </div>
-            <div class="field">
-                <label class="label">MaThue</label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="e.g. barryallen@example.com" name="MaThue">
+
+                <div class="inputBox p-3">
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Thời gian (tháng): </label>
+                        <div class="control">
+                            <select id="timeInut" class="form-control">
+
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Loại thiết bị: </label>
+                        <div class="control">
+                            <select id="orderTypeInput" class="form-control">
+                                <option value=0>Không</option>
+                                <option value=1>Doanh nghiệp</option>
+                                <option value=2>Cá nhân</option>
+                                <option value=3>Hộ kinh doanh</option>
+                            </select>
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Giá thiết bị: </label>
+                        <div class="control">
+                            <input class="input form-control" disabled type="text" name="giathietbi">
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Giá trước thuế: </label>
+                        <div class="control">
+                            <input class="input form-control" disabled type="text" name="giatruocthue">
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">VAT: </label>
+                        <div class="control">
+                            <input class="input form-control" disabled type="text" name="giatruocthue">
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Giá sau thuế: </label>
+                        <div class="control">
+                            <input class="input form-control" disabled type="text" name="giasauthue">
+                        </div>
+                    </div>
+
+                    <div class="field mb-3 align-items-center justify-content-between d-flex">
+                        <label class="label me-2">Ghi chú: </label>
+                        <div class="control">
+                            <input class="input form-control" type="text" name="ghichu">
+                        </div>
+                    </div>
                 </div>
-                <p class="help is-danger">{{ $errors->first('MaThue') }}</p>
-            </div>
-            <div class="field">
-                <label class="label">MaBHXH</label>
-                <div class="control">
-                    <input class="input" type="text" placeholder="e.g. barryallen@example.com" name="MaBHXH">
-                </div>
-                <p class="help is-danger">{{ $errors->first('MaBHXH') }}</p>
             </div>
             <button class="btn btn-success" type="submit">submit</button>
         </form>
     </div>
 </body>
 <script>
-    $("#idForm").submit(function(e) {
+    var listProvince = []
+    var listWard = []
+    var listDistrict = []
+    var provinceSelected
+    var districtSelected
+    var wardSelected
 
+    //GET PROVINCE
+    $(document).ready(function() {
+        $.ajax({
+            type: "GET",
+            url: "https://provinces.open-api.vn/api/p/",
+            success: function(data) {
+                listProvince = data
+                console.log(data)
+                $.each(data, function(index, value) {
+                    $("#provinceInput").prepend(`<option value=${value.code}>${value.name}</option>`);
+                })
+            }
+        })
+    })
+
+    //GET DISTRICT
+    $(document).ready(function() {
+        $.ajax({
+            type: "GET",
+            url: "https://provinces.open-api.vn/api/d/",
+            success: function(data) {
+                listDistrict = data
+            }
+        })
+    })
+
+    //GET WARD
+    $(document).ready(function() {
+        $.ajax({
+            type: "GET",
+            url: "https://provinces.open-api.vn/api/w/",
+            success: function(data) {
+                listWard = data
+            }
+        })
+    })
+
+    //HANDLE CHANGE PROVINCE
+    $("#provinceInput").on("change", function(e) {
+        code = e.target.value
+        $("#districtInput").empty()
+
+        for (let i = 0; i < listProvince.length; i++) {
+            if (listProvince[i].code == code) {
+                for (let j = 0; j < listDistrict.length; j++) {
+                    if (listDistrict[j].province_code == code) {
+                        $("#districtInput").append(`<option value=${listDistrict[j].code}>${listDistrict[j].name}</option>`);
+                    }
+                }
+                provinceSelected = listProvince[i]
+                console.log(listProvince[i])
+                break;
+            }
+        }
+    })
+
+    //HANDLE CHANGE DISTRICT
+    $("#districtInput").on("change", function(e) {
+        code = e.target.value
+        $("#wardInput").empty()
+
+        for (let i = 0; i < listDistrict.length; i++) {
+            if (listDistrict[i].code == code) {
+                for (let j = 0; j < listWard.length; j++) {
+                    if (listWard[j].district_code == code) {
+                        $("#wardInput").append(`<option value=${listWard[j].code}>${listWard[j].name}</option>`);
+                    }
+                }
+                districtSelected = listDistrict[i]
+                console.log(districtSelected)
+                break;
+            }
+        }
+    })
+
+    //HANDLE CHANGE DISTRICT
+    $("#wardInput").on("change", function(e) {
+        code = e.target.value
+
+        for (let i = 0; i < listWard.length; i++) {
+            if (listWard[i].code == code) {
+                wardSelected = listWard[i]
+                console.log(wardSelected)
+                break;
+            }
+        }
+    })
+
+    $("#idForm").submit(function(e) {
         e.preventDefault(); // avoid to execute the actual submit of the form.
 
         var form = $(this);
--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" data-bs-theme="dark">
 
 <head>
   <meta charset="UTF-8">
@@ -9,22 +9,168 @@
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.0/jquery.min.js" integrity="sha512-3gJwYpMe3QewGELv8k/BX9vcqhryRdzRMxVfq6ngyWXwo03GFEzjsUm8Q7RZcHPHksttq7/GFoxjCVUjkjvPdw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+
+  <link href="https://cdn.datatables.net/v/bs5/dt-1.13.5/sl-1.7.0/datatables.min.css" rel="stylesheet">
+  <script src="https://cdn.datatables.net/v/bs5/dt-1.13.5/sl-1.7.0/datatables.min.js"></script>
+
+  <link rel="stylesheet" href="/css/app.css">
+  <link rel="stylesheet" href="/css/dataTable.css">
 </head>
 
 <body>
-  <button class="btn">click</button>
-  <ul id="content"></ul>
+  <div class="container">
+    <table id="contracts" class="table table-striped" class="display">
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Tên khách hàng</th>
+          <th>Địa chỉ</th>
+          <th>Mã thuế</th>
+          <th>Mã bảo hiểm xã hội</th>
+          <th>Thao tác</th>
+        </tr>
+      </thead>
+      <tbody>
+      </tbody>
+    </table>
+
+    <!-- modal for view detail -->
+    <div class="modal fade" id="viewDetailModal" tabindex="-1" aria-labelledby="viewDetailModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="viewDetailModalLabel">Chi tiết khách hàng</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p><strong>Tên khách hàng:</strong> <span id="viewTenKH"></span></p>
+            <p><strong>Địa chỉ:</strong> <span id="viewDiaChi"></span></p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-primary" id="btnEdit">Sửa</button>
+            <button type="button" class="btn btn-danger" id="btnDelete">Xóa</button>
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal for Edit -->
+    <div class="modal fade" id="editModal" tabindex="-1" aria-labelledby="editModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="editModalLabel">Chỉnh sửa khách hàng</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <!-- Place your form elements here for editing customer details -->
+            <!-- For contracts: -->
+            <div class="form-group">
+              <label for="inputTenKH">Tên khách hàng</label>
+              <input type="text" class="form-control" id="inputTenKH">
+            </div>
+            <div class="form-group">
+              <label for="inputDiaChi">Địa chỉ</label>
+              <input type="text" class="form-control" id="inputDiaChi">
+            </div>
+            <!-- Add other form fields as needed -->
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Đóng</button>
+            <button type="button" class="btn btn-primary" id="btnSaveChanges">Lưu thay đổi</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Modal for Delete Confirmation -->
+    <div class="modal fade" id="deleteConfirmationModal" tabindex="-1" aria-labelledby="deleteConfirmationModalLabel" aria-hidden="true">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="deleteConfirmationModalLabel">Xác nhận xóa</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <p>Bạn có chắc chắn muốn xóa khách hàng này?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Hủy</button>
+            <button type="button" class="btn btn-danger" id="btnConfirmDelete">Xóa</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 
   <script>
-    const fetchApi = () => {
-      $.get("http://127.0.0.1:8000/api/customer", function(data, status) {
-        console.log(data)
-        $.each(data.data, function(index, value) {
-          $("#content").prepend(`<li>${value.TenKH}</li>`)
-        });
+    $(document).ready(function() {
+      $.ajax({
+        url: "http://127.0.0.1:8000/api/customer",
+        type: "GET",
+        success: function(data) {
+          console.log(data)
+          if (data.data) {
+            var contracts = data.data
+            $("#contracts").dataTable({
+              data: contracts,
+              ordering: true,
+              select: true,
+              columns: [{
+                  data: "id"
+                },
+                {
+                  data: "TenKH"
+                },
+                {
+                  data: "DiaChi"
+                },
+                {
+                  data: "MaThue"
+                },
+                {
+                  data: "MaBHXH"
+                },
+                {
+                  data: null,
+                  defaultContent: '<button type="button" class="btn btn-detail btn-info">Xem chi tiết</button>'
+                }
+              ]
+            });
+
+            $("#contracts").on("click", ".btn-detail", function() {
+              var data = $("#contracts").DataTable().row($(this).parents("tr")).data();
+              $("#viewTenKH").text(data.TenKH);
+              $("#viewDiaChi").text(data.DiaChi);
+              $("#viewDetailModal").modal("show");
+
+              // handle edit btn
+              $("#btnEdit").on("click", function() {
+                var dataEdit = $("#contracts").DataTable().row($("#contracts tr.selected")).data();
+
+                $("#inputTenKH").val("tên khách hàng");
+                $("#inputDiaChi").val("địa chỉ");
+                $("#editModal").modal("show");
+              });
+
+              // handle delete btn
+              $("#btnDelete").on("click", function() {
+                var dataDelete = $("#contracts").DataTable().row($("#contracts tr.selected")).data();
+                $("#deleteConfirmationModal").modal("show");
+
+                $("#btnConfirmDelete").on("click", function() {
+                  console.log(data.id)
+                  $("#deleteConfirmationModal").modal("hide");
+                  $("#viewDetailModal").modal("hide");
+                  var dataDeleted = $("#contracts").DataTable().row($("#contracts tr.selected")).data();
+                });
+              });
+            });
+          }
+        }
       });
-    }
-    fetchApi()
+    });
   </script>
 </body>
 
