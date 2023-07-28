0a4f2040471c3aece449eeffd09a5854b36db1fe
XDanh <danhb1805680@student.ctu.edu.vn>
2023-07-28 23:28:32 +0700

    av

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvi
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
+use Laravel\Fortify\Http\Controllers\AuthenticatedSessionController;
 
 class RouteServiceProvider extends ServiceProvider
 {
@@ -34,6 +35,9 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
 
+            Route::middleware(['web', 'auth'])
+                ->get('/logout', [AuthenticatedSessionController::class, 'destroy'])
+                ->name('logout');
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
         });
